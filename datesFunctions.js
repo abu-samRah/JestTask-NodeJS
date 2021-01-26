8e8f92828095cb0b819b4f37f3bc06980a6eee7c
abu-samRah <abdallahabuamrah@gmail.com>
2021-01-26 15:30:11 +0300

    uploading task

---
+++ datesFunctions.js
@@ -0,0 +1,143 @@
+function fromFormat(dateString, dateFormat) {
+    if (!dateString || !dateFormat) {
+        throw Error("Wrong Input");
+    }
+    const dayStartIndex = dateFormat.indexOf("d");
+    const dayLastIndex = dateFormat.lastIndexOf("d");
+    const monthStartIndex = dateFormat.indexOf("m");
+    const monthLastIndex = dateFormat.lastIndexOf("m");
+    const yearStartIndex = dateFormat.indexOf("y");
+    const yearLastIndex = dateFormat.lastIndexOf("y");
+
+    if (monthStartIndex == -1 || dayStartIndex == -1 || yearStartIndex == -1) {
+        throw Error("Wrong Input");
+    }
+
+
+    let startIndex = Math.min(dayStartIndex, monthStartIndex, yearStartIndex);
+    let endIndex = Math.max(dayLastIndex, monthLastIndex, yearLastIndex);
+    let afterSliceDateFormat = dateFormat.slice(0, startIndex) + dateFormat.slice(endIndex + 1, dateFormat.length);
+    let afterSliceDateString = dateString.slice(0, startIndex) + dateString.slice(endIndex + 4, dateString.length);
+
+    finalDate = {}
+    finalDate["year"] = (dateString.slice(yearStartIndex, yearLastIndex + 4));
+    const isNumOnly = /^\d+$/.test(finalDate["year"])
+
+    if (afterSliceDateFormat != afterSliceDateString || finalDate["year"].length != 4 || !isNumOnly) {
+        throw Error("Wrong Input");
+    }
+
+    finalDate["year"] = Number(finalDate["year"]);
+
+
+    if (yearStartIndex > dayStartIndex && yearStartIndex < monthStartIndex) {
+
+        finalDate["day"] = Number(dateString.slice(dayStartIndex, dayLastIndex + 1));
+        finalDate["month"] = Number(dateString.slice(monthStartIndex + 3, monthLastIndex + 4));
+
+    } else if (yearStartIndex > monthStartIndex && yearStartIndex < dayStartIndex) {
+
+        finalDate["day"] = Number(dateString.slice(dayStartIndex + 3, dayLastIndex + 4));
+        finalDate["month"] = Number(dateString.slice(monthStartIndex, monthLastIndex + 1));
+
+    } else if (yearStartIndex < monthStartIndex && yearStartIndex < dayStartIndex) {
+
+        finalDate["day"] = Number(dateString.slice(dayStartIndex + 3, dayLastIndex + 4));
+        finalDate["month"] = Number(dateString.slice(monthStartIndex + 3, monthLastIndex + 4));
+
+    } else {
+        finalDate["day"] = Number(dateString.slice(dayStartIndex, dayLastIndex + 1));
+        finalDate["month"] = Number(dateString.slice(monthStartIndex, monthLastIndex + 1));
+    }
+
+
+
+    return new Date(finalDate.year, finalDate.month - 1, finalDate.day).toString();
+}
+
+
+String.prototype.replaceBetween = function(start, end, str) {
+    return this.substring(0, start) + str + this.substring(end);
+};
+
+String.prototype.getPosition = function(subString, index) {
+    return this.split(subString, index).join(subString).length;
+}
+
+
+
+
+function format(dateObject, outputFormat) {
+    if (!dateObject || !outputFormat) {
+        throw Error("Wrong Input");
+    }
+
+    const monthsFullName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const monthsAbbrevName = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+    let day = dateObject.getDate();
+    let month = dateObject.getMonth();
+    let year = dateObject.getFullYear();
+
+    const dayStartIndex = outputFormat.toLowerCase().indexOf("d");
+    const dayLastIndex = outputFormat.toLowerCase().lastIndexOf("d");
+    const monthStartIndex = outputFormat.indexOf("M");
+    const monthLastIndex = outputFormat.lastIndexOf("M");
+    const yearStartIndex = outputFormat.toLowerCase().indexOf("y");
+    const yearLastIndex = outputFormat.toLowerCase().lastIndexOf("y");
+
+
+
+    if (monthStartIndex == -1 || dayStartIndex == -1 || yearStartIndex == -1) {
+        throw Error("Wrong Input");
+    }
+
+    const dayIndexDiff = dayLastIndex - dayStartIndex;
+    let dayFinal = dayIndexDiff == 0 || (dayIndexDiff == 1 && day > 9) ? day : (dayIndexDiff == 1 && day < 10 ? "0" + day : "Wrong Day");
+
+    const monthIndexDiff = monthLastIndex - monthStartIndex;
+    let monthFinal = monthIndexDiff == 0 || (monthIndexDiff == 1 && month > 9) ? month + 1 : (monthIndexDiff == 1 && month < 10 ? "0" + (month + 1) : (monthIndexDiff == 2 ? monthsAbbrevName[month] : (monthIndexDiff == 3 ? monthsFullName[month] : "Wrong Month")));
+
+    const yearIndexDiff = yearLastIndex - yearStartIndex;
+    let yearFinal = yearIndexDiff == 0 || (yearIndexDiff == 3) ? year : (yearIndexDiff == 1 ? year % 100 : "Wrong Year");
+
+    const errormessage = dayFinal == "Wrong Day" ? dayFinal : (monthFinal == "Wrong Month" ? monthFinal : (yearFinal == "Wrong Year" ? "Wrong Year" : "No Errors"));
+    if (errormessage != "No Errors") {
+        throw Error(errormessage);
+    }
+
+    const replacemonth = outputFormat.replaceBetween(monthStartIndex, monthLastIndex + 1, monthFinal);
+
+    const dayStartReplaceIndex = replacemonth.toLowerCase().indexOf("d");
+    const dayEndReplaceIndex = replacemonth.toLowerCase().lastIndexOf("d");
+
+    const replaceday = replacemonth.replaceBetween(dayStartReplaceIndex, dayEndReplaceIndex + 1, dayFinal);
+
+
+    let yearStartReplaceIndex = 0;
+    let yearEndReplaceIndex = 0;
+
+
+    if ((yearStartIndex > monthStartIndex) && (monthFinal == "January" || monthFinal == "February" || monthFinal == "May" || monthFinal == "July")) {
+        yearStartReplaceIndex = replaceday.toLowerCase().getPosition("y", 2);
+        yearEndReplaceIndex = replaceday.toLowerCase().lastIndexOf("y");
+    } else {
+        yearStartReplaceIndex = replaceday.toLowerCase().indexOf("y");
+        yearEndReplaceIndex = yearIndexDiff == 3 ? replaceday.toLowerCase().getPosition("y", 4) : (yearIndexDiff == 1 ? replaceday.toLowerCase().getPosition("y", 2) : yearStartReplaceIndex);
+    }
+
+
+
+    const finalReplace = replaceday.replaceBetween(yearStartReplaceIndex, yearEndReplaceIndex + 1, yearFinal)
+    return finalReplace;
+
+
+}
+
+
+
+
+module.exports = {
+    fromFormat: fromFormat,
+    format: format,
+};
---
+++ datesFunctions.test.js
@@ -0,0 +1,168 @@
+const dates = require('./datesFunctions.js');
+
+describe('Testing fromFormat function', () => {
+    const expected = "Thu Dec 10 2009 00:00:00 GMT+0300 (Arabian Standard Time)"
+
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("10/12/2009", "dd/mm/y");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("10/12/2009", "dd-mm-y");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("2009/12/10", "y/mm/dd");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("12/2009/10", "mm/y/dd");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("2009/10/12", "y/dd/mm");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("10/2009/12", "dd/y/mm");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("hi, 10/12/2009", "hi, dd/mm/y");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return valid date object when passing valid date format', () => {
+        let result = dates.fromFormat("hi7777, 10/12/2009xx", "hi7777, dd/mm/yxx");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return error message', () => {
+        let expected = "Wrong Input";
+        let result = () => dates.fromFormat("10/12/2", "dd/mm/y");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        let expected = "Wrong Input";
+        let result = () => dates.fromFormat("hi77, 10/12/2009", "hi, dd/mm/y");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        let expected = "Wrong Input"
+        let result = () => dates.fromFormat("10/12/209", "dd/mm/y");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        let expected = "Wrong Input"
+        let result = () => dates.fromFormat("10/12/2", "dd/mm/y");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+
+        expect(() => dates.fromFormat()).toThrowError("Wrong Input");
+    });
+
+
+
+});
+
+
+describe('Testing format function', () => {
+    const format = dates.format;
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "01-26/2021"
+        const result = format(new Date(), "MM-DD/YYYY");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "Jan-26/2021"
+        const result = format(new Date(), "MMM-D/YYYY");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "2021-26/January"
+        const result = format(new Date(), "YYYY-D/MMMM");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "26/2021-January"
+        const result = format(new Date(), "D/YYYY-MMMM");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "26/21-January"
+        const result = format(new Date(), "D/YY-MMMM");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "21/26/-January"
+        const result = format(new Date(), "YY/D/-MMMM");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "HI: 21/26/-January"
+        const result = format(new Date(), "HI: YY/D/-MMMM");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return a date formatted string when passing valid date object', () => {
+        const expected = "2021/26/-January"
+        const result = format(new Date(), "Y/D/-MMMM");
+        expect(result).toEqual(expected);
+    });
+
+    test('Should return error message', () => {
+        const expected = "Wrong Input"
+        const result = () => format(new Date(), "HI: YY/xx/-MMMM");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        const expected = "Wrong Month"
+        const result = () => format(new Date(), "HI: My/DD/-MMMM");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        const expected = "Wrong Day"
+        const result = () => format(new Date(), "y/DDD/-MMMM");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        const expected = "Wrong Year"
+        const result = () => format(new Date(), "yyy/DD/-MMMM");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        const expected = "Wrong Month"
+        const result = () => format(new Date(), "yyy/DD/-MMMMM");
+        expect(result).toThrowError(expected);
+    });
+
+    test('Should return error message', () => {
+        const expected = "Wrong Input"
+        const result = () => format();
+        expect(result).toThrowError(expected);
+    });
+
+
+});
